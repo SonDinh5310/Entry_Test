be8d8eb03d86c80c618344686173238fe675bbbf
Son Dinh <dnson20022001@gmail.com>
2020-11-03 21:44:19 +0700

    update code

--- codeChallenge.js
+++ codeChallenge.js
@@ -21,6 +21,7 @@ btnSubmit.addEventListener("click", () => {
   let input = parseInt(inputFieldElement.value);
   if (validateInput(1, 10, input) === -1) {
     alert("Ban da nhap sai!!! Hay nhap lai");
+    inputFieldElement.value = "";
     return;
   } else {
     if (input === result) {
